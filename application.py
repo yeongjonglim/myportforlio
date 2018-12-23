389b6ba8d25752fa94caef295d246c18087787ed
YJ Lim <voltendron@YJs-MacBook-Pro.local>
2018-12-23 19:19:03 +0000

    updated directory

--- application.py
+++ application.py
@@ -162,7 +162,7 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("/login.html")
 
 
 @app.route("/logout")
