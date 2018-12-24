76b1c50db5005c00493b5b7fcd0cf692f8f59361
YJ Lim <voltendron@YJs-MacBook-Pro.local>
2018-12-24 22:16:40 +0000

    making it better

--- application.py
+++ application.py
@@ -31,11 +31,11 @@ app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["DEBUG"] = False
 app.config["TESTING"] = False
-app.config["SECRET_KEY"] = os.urandom(24)
+app.config["SECRET_KEY"] = '\xda\x1d\xc8y\xc8\xcb5\xfb\xee\xc6r6\xe4\xfc\xc4\xa9\x16\x81\xf0v\xc6\  x86\xc3\x96'
 #app.config["SESSION_NAME"] = 'session name'
 app.secret_key = '\xda\x1d\xc8y\xc8\xcb5\xfb\xee\xc6r6\xe4\xfc\xc4\xa9\x16\x81\xf0v\xc6\ x86\xc3\x96'
 Session(app)
