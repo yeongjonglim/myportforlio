4ebf7a7eed2ca6f564c16849e6b9573f6ce70017
YJ Lim <voltendron@YJs-MacBook-Pro.local>
2018-12-24 19:24:59 +0000

    Testing on session issue

--- financeHelpers.py
+++ financeHelpers.py
@@ -28,7 +28,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session['user_id'] is None:
+        if session.get('user_id') is None:
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
