68ca5e0e7c64df5be41b16c510132c788d9e3661
YJ Lim <voltendron@YJs-MacBook-Pro.local>
2018-12-27 21:26:18 +0000

    config update

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ from financeHelpers import apology, login_required, lookup, usd, checkpwstr
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+#app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
 @app.after_request
